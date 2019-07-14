ed34d0a9239449c14818b0e720a71eaa2204dbc4
Takeshi Iwana <iwatakeshi@gmail.com>
2019-07-13 23:34:54 -0600

    Release as web component

--- src/components/Vanta/Vanta.vue
+++ src/components/Vanta/Vanta.vue
@@ -7,7 +7,7 @@
 <script>
 
 export default {
-  name: 'vanta',
+  name: 'vue-vanta',
   props: {
     effect: {
       type: String,
--- src/main.js
+++ src/main.js
@@ -3,11 +3,9 @@ import * as components from './components'
 const ComponentLibary = {
   install(Vue) {
     // components
-    for (const componentName in components) {
-      const component = components[componentName]
-
-      Vue.component(component.name, component)
-    }
+    Object.keys(components).forEach(name => {
+      Vue.component(name, components[name])
+    })
   }
 }
 
